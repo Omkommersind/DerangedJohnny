a65e8fc5f6646e451135c22265c8568ce1604807
Omkommersind <chauzov@list.ru>
2019-06-09 17:37:31 +0300

    Charecter name

--- Assets/Scripts/Deprecated/Johnny.cs
+++ Assets/Scripts/Deprecated/Johnny.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Scrooge : BaseCharecter
+public class Johnny : BaseCharecter
 {
     public enum StatesEnum { Idle, Run, Jump, Shoot }
     public StatesEnum State
