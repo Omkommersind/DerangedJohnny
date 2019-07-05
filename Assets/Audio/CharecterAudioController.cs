f9f85d8ba7cbfce6df36fc4f82fc458bab9f144a
Omkommersind <chauzov@list.ru>
2019-07-05 22:43:00 +0300

    Sounds

---
+++ Assets/Audio/CharecterAudioController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharecterAudioController : MonoBehaviour
+{
+    public AudioClip StepClip;
+    public AudioClip JumpClip;
+    public AudioClip LandClip;
+
+    private AudioSource _audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+        if (StepClip == null)
+        {
+            Debug.LogError("Audio clip for StepClip not provided");
+        }
+    }
+
+    private void Step()
+    {
+        _audioSource.PlayOneShot(StepClip);
+    }
+}
